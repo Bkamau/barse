bcf1772882e5e0d50681e1cd0a80f269c2e92610
vaxly <benson@vaxly.io>
2016-07-16 07:43:10 +0300

    it's :running:

--- scripts/copy-dir.sh
+++ scripts/copy-dir.sh
@@ -9,17 +9,26 @@ sudo cp -rf ../ParseServer /usr/src/apps
 sudo cp -rf ../Caddy/caddy /usr/bin
 sudo cp -rf ../Caddy/Caddyfile /etc/caddy
 sudo cp -rf ../Caddy/caddy.service /etc/systemd/system
+sudo cp -rf ../Mongodb/mongodb.service /etc/systemd/system
 
+# Enable & start mongo server
+sudo systemctl enable mongodb.service
+sudo systemctl start mongodb.service
+
+#Install server
 cd /usr/src/apps/ParseServer
 sudo chown -R ubuntu /usr/src/apps
 sudo npm install
 
+# Provision server
 pm2 start /usr/src/apps/ParseServer/start.sh --name="ParseServer"
 pm2 startup ubuntu
 sudo su -c "env PATH=$PATH:/usr/bin pm2 startup ubuntu -u ubuntu --hp /home/ubuntu"
 pm2 save
 
+# Enable & start caddy web server
 sudo systemctl enable caddy.service
 sudo systemctl start caddy.service
 
+# Enjoy
 echo "Wait 30  secs for server to start"
