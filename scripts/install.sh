d096dd3300b1b29b0b30a2badd8c68077e2744a2
vaxly <benson@vaxly.io>
2016-07-16 08:05:15 +0300

    Always :running:

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,8 +18,8 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install MongoDB
-sudo apt adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 -y
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt -y update 
 sudo apt -y install --allow-unauthenticated mongodb-org
 
